f9062a581ce973c212c9bc5ee9bfe3e5ea7fb6f9
feitnomore <feitnomore@users.noreply.github.com>
2022-12-14 16:10:21 -0300

    SEO js

---
+++ js/organization.js
@@ -0,0 +1,7 @@
+{
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "kubevirt-manager",
+    "url": "https://kubevirt-manager.io/",
+    "logo": "https://kubevirt-manager.io/img/kubevirt-manager-logo.png"
+}
